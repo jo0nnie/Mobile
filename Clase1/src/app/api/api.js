87238459aa92c4fd5d60339eb92e9fe2432d849c
Lucrecia Galarza <44737561@ROQUEGONZALEZ.ORG>
2025-09-28 11:08:30 -0300

    fix: navegación y perfil modularizados

--- Clase1/src/app/api/api.js
+++ Clase1/src/app/api/api.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
-const POKE_URL = "https://pokeapi.co/api/v2";
-const BASE_URL = "https://template-express-api.onrender.com";
+//const POKE_URL = "https://pokeapi.co/api/v2";
+//const BASE_URL = "https://template-express-api.onrender.com";
+const PELI_URL = "https://api.tvmaze.com/";
 
 export const pokeApi = axios.create({
-  baseURL: POKE_URL,
+  baseURL: PELI_URL,
   timeout: 5000,
 });
 
 export const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: PELI_URL,
   timeout: 5000,
 });
--- Clase1/src/app/users/[id].js
+++ Clase1/src/app/users/[id].js
@@ -1,47 +1,12 @@
-import React, { useEffect } from "react";
-import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
-import { useSearchParams } from "expo-router";
-import { useUsers } from "../../hooks/useUsers";
+import { Text, View } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
 
 export default function UserDetail() {
-  const { id } = useSearchParams();
-  const { selectedUser, loading, error, fetchUserById } = useUsers();
-
-  useEffect(() => {
-    if (id) fetchUserById(id);
-  }, [id]);
-
-  if (loading) return <ActivityIndicator size="large" color="blue" />;
-  if (error) return <Text style={styles.error}>{error}</Text>;
-  if (!selectedUser) return <Text>No se encontró el usuario</Text>;
+  const { id } = useLocalSearchParams();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Name:</Text>
-      <Text style={styles.value}>{selectedUser.name}</Text>
-
-      <Text style={styles.label}>Username:</Text>
-      <Text style={styles.value}>{selectedUser.username}</Text>
+    <View>
+      <Text>Detalle del usuario: {id}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    justifyContent: "center",
-  },
-  label: {
-    fontWeight: "bold",
-    fontSize: 18,
-    marginTop: 10,
-  },
-  value: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  error: {
-    color: "red",
-  },
-});
